3005c69b66feb7a407b323d8867a4af32dc3bcd4
Meddy <meddycond@gmail.com>
2023-12-04 19:40:51 -0600

    Initial Commit

---
+++ HKRNWS/HKRNWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKRNWSApp.swift
+//  HKRNWS
+//
+//  Created by Meddy on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HKRNWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
